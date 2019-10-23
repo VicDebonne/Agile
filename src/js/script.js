950b72970db22a3150e296b02a822806bd7c0134
ryantytgat <ryan.tytgat@student.howest.be>
2019-10-23 15:45:14 +0200

    javascript progress

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,27 @@ function guardarNumeros(){
  console.log(items);
  return false;
 }
+
+{
+  let quotes = [];
+
+  const init = () => {
+    const $addbutton = document.querySelector(`.addbutton`);
+    $addbutton.onclick = () => getValue();
+  };
+
+  const getValue = () => {
+    const $invoerveld = document.querySelector(`.invoerveld`);
+    const invoer = $invoerveld.value;
+
+    quotes.push(invoer);
+    showResult(quotes);
+  };
+
+  const showResult = result => {
+    const $resultfield = document.querySelector(`.result`);
+    $resultfield.textContent = result;
+  };
+
+  init();
+}
